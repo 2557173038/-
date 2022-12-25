063310ea0cae4cba946983407dd083ac4fede6b3
小企鹅 <2557173038@qq.com>
2022-12-25 14:54:11 +0800

    12.25

--- 前端面试题1/02-codeResult-1.js
+++ 前端面试题1/02-codeResult-1.js
@@ -1,11 +1,15 @@
+//同名
+
 var a=1
 {
     function a(){}
     a=2
     function a(){}
-    a=3
+    a=4 //挤出块外
     function a(){}
-    a=4
+    a=6
     console.log('内部a',a)
 }
-console.log('外部a',a)
+console.log('外部a',a)//内部最后一个函数前面的变量值
+
+// 6,4
---
+++ 前端面试题1/03-lietive/src/App.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <div>
+    <p >666</p>
+   <p ref="p"> {{a}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      a: 100,
+    };
+  },
+  methods:{
+    handleClick(){
+}
+  },
+ 
+  beforeCreate() {},
+  created(){
+    console.log(this.a)
+    console.log(this.handleClick)
+    console.log(this.$el) //dom节点
+
+  },
+  async mounted(){
+    console.log(this.$el)
+    this.a=200
+    // this.$nextTick(()=>{ //在改变变量后操作dom
+    // console.log(this.$refs.p.innerHTML)
+    // })
+      await this.$nextTick() //当节点全部挂载完成后操作dom
+    console.log(this.$refs.p.innerHTML) 
+  },
+  Update(){
+    console.log(this.a)
+  }
+ 
+};
+</script>
+
